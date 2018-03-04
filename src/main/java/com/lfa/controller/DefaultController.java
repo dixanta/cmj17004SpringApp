6a5311a7877db3c420e8f0a1fd8a7102f0ab6ab6
dixanta <dixanta@gmail.com>
2018-03-04 10:13:45 +0545

    heroku upload

---
+++ src/main/java/com/lfa/controller/DefaultController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lfa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author USER
+ */
+@Controller
+@RequestMapping(value="/home")
+public class DefaultController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model){
+        model.addAttribute("name","Spring Web Project");
+        return "home/index";
+    }
+}
---
+++ src/main/java/com/lfa/controller/admin/DashboardController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lfa.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author USER
+ */
+@Controller
+@RequestMapping(value="/admin")
+public class DashboardController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(){
+        return "admin/dashboard/index";
+    }
+}
